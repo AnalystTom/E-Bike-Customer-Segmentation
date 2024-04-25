d0356b983b469bc0f1a0721cccc435d480e3f458
RusskiBoyVlad <thomasmann2001@gmail.com>
2024-04-25 17:52:16 +0100

    added query example

--- queries.py
+++ queries.py
@@ -3,17 +3,14 @@ import duckdb
 # Connect to DuckDB. If the database does not exist, it will be created.
 con = duckdb.connect(database='transaction.db', read_only=False)
 
-# SQL statement to create the 'transactions' table
-create_table_sql = """
-    SELECT * FROM transactions;
-
+# SQL statement to calculate customers per postcode
+Postcode_metrics = """
+    CREATE OR REPLACE VIEW v1 AS
+    SELECT SUM(customer_id)
+    FROM customers
+    JOIN addresses ON customers.customer_id = addresses.customer_id
+    GROUP BY addresses.postcode;
 """
-
-# Execute the SQL statement to create the table
-con.execute(create_table_sql)
-
-# Verify the table was created by listing tables in the database
-
 
 # Close the connection
 con.close()
